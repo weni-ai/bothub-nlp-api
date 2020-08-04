91fcdfb96dcf9fcd3238681f741f1e275e9c790b
Lucas Raggi <lrr@ic.ufal.br>
2020-08-04 15:27:59 -0300

    change variable name

--- bothub_nlp_api/handlers/train.py
+++ bothub_nlp_api/handlers/train.py
@@ -54,7 +54,7 @@ def train_handler(authorization, repository_version=None):
                 by_id=str(current_update.get("repository_authorization_user_id")),
                 repository_authorization=str(repository_authorization),
                 language=language,
-                type_model=ALGORITHM_TO_LANGUAGE_MODEL[algorithm]
+                type_model=ALGORITHM_TO_LANGUAGE_MODEL[chosen_algorithm]
             )
             backend().request_backend_save_queue_id(
                 update_id=str(current_update.get("current_version_id")),
