cb3039b47ddefa1b1ed7b23d6d8e900df681a89d
Daniel Yohan <dyohan9@gmail.com>
2020-10-05 15:52:35 -0300

    Added validation if intent_ranking is None

--- bothub_nlp_api/handlers/parse.py
+++ bothub_nlp_api/handlers/parse.py
@@ -1,6 +1,5 @@
 import json
 import threading
-import logging
 import re
 
 from bothub_nlp_celery.actions import ACTION_PARSE, queue_name
@@ -132,19 +131,8 @@ def _parse(
         }
     )
 
-    try:
-        log_intent = []
-        for result in answer.get("intent_ranking", []):
-            log_intent.append(
-                {
-                    "intent": result["name"],
-                    "is_default": result["name"] == answer["intent"]["name"],
-                    "confidence": result["confidence"],
-                }
-            )
-    except Exception as err:
-        logging.error(f"Unknown error log_intent {err}")
-        log_intent = []
+    if "intent_ranking" not in answer or answer.get("intent_ranking") is None:
+        answer.update({"intent_ranking": []})
 
     log = threading.Thread(
         target=backend().send_log_nlp_parse,
@@ -156,7 +144,14 @@ def _parse(
                 "user": str(get_repository_authorization(authorization)),
                 "repository_version_language": int(update.get("repository_version")),
                 "nlp_log": json.dumps(answer),
-                "log_intent": log_intent,
+                "log_intent": [
+                    {
+                        "intent": result["name"],
+                        "is_default": result["name"] == answer["intent"]["name"],
+                        "confidence": result["confidence"],
+                    }
+                    for result in answer.get("intent_ranking", [])
+                ],
             }
         },
     )
