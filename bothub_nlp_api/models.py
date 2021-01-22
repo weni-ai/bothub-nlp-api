96ff031f55422c98fa90fe43fac97b30b2ad0d55
Lucas Agra <lucasomena@hotmail.com>
2021-01-22 17:48:12 -0300

    undo commit

--- bothub_nlp_api/models.py
+++ bothub_nlp_api/models.py
@@ -91,10 +91,6 @@ class SentenceSuggestionResponse(BaseModel):
 class IntentSentenceSuggestionResponse(BaseModel):
     intent: str
     suggested_sentences: List[str]
-    repository_version: int = None
-    language: str
-    n_sentences_to_generate: int
-    percentage_to_replace: float
 
 
 class WordSuggestionResponse(BaseModel):
