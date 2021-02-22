44a71c55ad8a654271c2eaaed216f19eb4efd488
Lucas Agra <lucasomena@hotmail.com>
2021-02-22 15:37:40 -0300

    qa_model initializing

--- bothub_nlp_api/handlers/question_answering.py
+++ bothub_nlp_api/handlers/question_answering.py
@@ -4,6 +4,7 @@ from bothub_nlp_celery.actions import ACTION_QUESTION_ANSWERING, queue_name
 
 
 def qa_handler(context, question, language):
+    # TODO: Repository Authentication
 
     answer_task = celery_app.send_task(
         TASK_NLU_QUESTION_ANSWERING,
