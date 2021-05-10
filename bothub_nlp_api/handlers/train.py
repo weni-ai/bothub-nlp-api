73a22c8007c85d4424ceb2a6569f4a6382085eab
Lucas Agra <lucasomena@hotmail.com>
2021-05-10 18:56:04 -0300

    Refactoring variable update

--- bothub_nlp_api/handlers/train.py
+++ bothub_nlp_api/handlers/train.py
@@ -27,39 +27,39 @@ def train_handler(authorization, repository_version=None, language=None):
             repository_authorization, repository_version
         )
 
-    for update in ready_to_train_languages:
+    for repository in ready_to_train_languages:
 
-        model = get_language_model(update)
+        model = get_language_model(repository)
         if settings.BOTHUB_SERVICE_TRAIN == "celery":
             train_task = celery_app.send_task(
                 TASK_NLU_TRAIN_UPDATE,
                 args=[
-                    update.get("current_version_id"),
-                    update.get("repository_authorization_user_id"),
+                    repository.get("current_version_id"),
+                    repository.get("repository_authorization_user_id"),
                     repository_authorization,
                 ],
-                queue=queue_name(update.get("language"), ACTION_TRAIN, model),
+                queue=queue_name(repository.get("language"), ACTION_TRAIN, model),
             )
-            train_tasks.append({"task": train_task, "language": update.get("language")})
+            train_tasks.append({"task": train_task, "language": repository.get("language")})
         elif settings.BOTHUB_SERVICE_TRAIN == "ai-platform":
-            job_id = f'bothub_{settings.ENVIRONMENT}_train_{str(update.get("current_version_id"))}_{update.get("language")}_{str(int(time.time()))}'
+            job_id = f'bothub_{settings.ENVIRONMENT}_train_{str(repository.get("current_version_id"))}_{repository.get("language")}_{str(int(time.time()))}'
             send_job_train_ai_platform(
                 jobId=job_id,
-                repository_version=str(update.get("current_version_id")),
-                by_id=str(update.get("repository_authorization_user_id")),
+                repository_version=str(repository.get("current_version_id")),
+                by_id=str(repository.get("repository_authorization_user_id")),
                 repository_authorization=str(repository_authorization),
-                language=update.get("language"),
+                language=repository.get("language"),
                 type_model=model,
                 operation="train",
             )
             backend().request_backend_save_queue_id(
-                update_id=str(update.get("current_version_id")),
+                update_id=str(repository.get("current_version_id")),
                 repository_authorization=str(repository_authorization),
                 task_id=job_id,
                 from_queue=0,
                 type_processing=0,
             )
-        languages_report[update.get("language")] = {"status": TRAIN_STATUS_PROCESSING}
+        languages_report[repository.get("language")] = {"status": TRAIN_STATUS_PROCESSING}
 
     resp = {
         "SUPPORTED_LANGUAGES": list(settings.SUPPORTED_LANGUAGES.keys()),
