00f79e27be4d42dead791b1658086bde6a5efc0e
John Cordeiro <johncordeiro@MacBook-Air-de-John.local>
2022-09-13 10:43:38 -0300

    Fix slow API

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 cd $WORKDIR
 
 #gunicorn bothub_nlp_api.wsgi --log-level debug --timeout 999999 -c gunicorn.conf.py
-gunicorn -k uvicorn.workers.UvicornWorker --timeout 999999 -c "gunicorn.conf.py" "bothub_nlp_api.app:app"
+#gunicorn -k uvicorn.workers.UvicornWorker --timeout 999999 -c "gunicorn.conf.py" "bothub_nlp_api.app:app"
+gunicorn -k uvicorn.workers.UvicornWorker --timeout 120 -c "gunicorn.conf.py" "bothub_nlp_api.app:app"
